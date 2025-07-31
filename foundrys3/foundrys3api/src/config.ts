8d075d5abdac94b847559e62b8f53cee723d279e
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-07-31 17:31:11 +0530

    feat: foundrys3 project

---
+++ foundrys3/foundrys3api/src/config.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { S3Client } from '@aws-sdk/client-s3';
+
+export const MAIN_QUEUE_URL = process.env.MAIN_QUEUE_URL;
+export const AWS_REGION = process.env.AWS_REGION;
+export const BUCKET_NAME = process.env.S3_BUCKET_NAME;
+
+export const s3Client = new S3Client({
+    region: AWS_REGION,
+});
